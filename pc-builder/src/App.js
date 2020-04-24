5baea5173440ca7831d267e16c95bc4d0e8cc50b
Gia Bao Nguyen <giabnguyen1@gmail.com>
2020-04-23 22:28:44 -0400

    Added classnames for css changes in App.js

--- pc-builder/src/App.js
+++ pc-builder/src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>PC Builder</h1>
-      <p>This website will provide information for building a computer</p>
-      <p>
-        Whether you are a novice or an expert, this site is user friendly and
-        will provide necessary information for building a computer
-      </p>
-      <p>
-        Computer components will be available to compare and at the end a price
-        rundown as well as total will be displayed
-      </p>
+      <h1 className="app-header">PC Builder</h1>
+      <div className="paragraph">
+        <p>This website will provide information for building a computer</p>
+        <p>
+          Whether you are a novice or an expert, this site is user friendly and
+          will provide necessary information for building a computer
+        </p>
+        <p>
+          Computer components will be available to compare and at the end a
+          price rundown as well as total will be displayed
+        </p>
+      </div>
     </div>
   );
 }
