2fd39a77ff960936986570d79a78869c34e232b1
Gia Bao Nguyen <giabnguyen1@gmail.com>
2020-06-29 13:15:12 -0400

    Completed base component to call Card component

--- pc-builder/src/App.js
+++ pc-builder/src/App.js
@@ -1,23 +1,50 @@
 import React from "react";
 import "./App.css";
+import Card from "./Card";
+import PriceRundown from "./PriceRundown";
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="app-header">PC Builder</h1>
-      <div className="paragraph">
-        <p>This website will provide information for building a computer</p>
-        <p>
-          Whether you are a novice or an expert, this site is user friendly and
-          will provide necessary information for building a computer
-        </p>
-        <p>
-          Computer components will be available to compare and at the end a
-          price rundown as well as total will be displayed
-        </p>
+class App extends React.Component {
+  render() {
+    let comps = [
+      "CPU",
+      "Motherboard",
+      "RAM",
+      "Case",
+      "Cooling",
+      "GPU",
+      "HDD",
+      "SSD",
+      "PSU",
+    ];
+    let prices = [
+      649.99,
+      234.08,
+      204.0,
+      199.99,
+      154.99,
+      800.0,
+      100.0,
+      255.0,
+      172.99,
+    ];
+    return (
+      <div className="App">
+        <h1 className="app-header">PC Builder</h1>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="container-fluid" style={{ marginLeft: "8em" }}>
+                <div className="row">
+                  {comps.map((comp) => {
+                    return <Card comp={comp} />;
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
