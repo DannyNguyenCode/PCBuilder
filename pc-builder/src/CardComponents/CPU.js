c8d0cfbc3663235ecae012a6e7ad925b5cd6cd7b
Gia Bao Nguyen <giabnguyen1@gmail.com>
2020-10-14 11:26:38 -0400

    filled out select tags with data

--- pc-builder/src/CardComponents/CPU.js
+++ pc-builder/src/CardComponents/CPU.js
@@ -1,21 +1,55 @@
 import React from "react";
 import Select from "react-select";
+import { filter } from "lodash";
 
 class CPU extends React.Component {
-  state = { selectedOption: null };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption1: "",
+      selectedOption2: "",
+      filteredProcessors: [{ label: "" }],
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      filterProcessors: this.props.value4,
+    });
+  }
   handleChange = (selectedOption) => {
-    this.setState({ selectedOption });
+    this.setState({ selectedOption1: selectedOption }, () =>
+      console.log(selectedOption)
+    );
+  };
+  filterData = () => {
+    if (this.state.selectedOption1 === "") {
+      return this.props.value4;
+    } else {
+      if (
+        this.state.selectedOption1.label.toLowerCase() === "intel".toLowerCase()
+      ) {
+        let filterProcessor = this.props.value4.filter((data) =>
+          data.label.toLowerCase().includes("intel".toLowerCase())
+        );
+        return filterProcessor;
+      } else {
+        let filterProcessor = this.props.value4.filter((data) =>
+          data.label.toLowerCase().includes("AMD".toLowerCase())
+        );
+        return filterProcessor;
+      }
+    }
   };
   render() {
-    const { selectedOption } = this.state;
+    const { selectedOption1 } = this.state.selectedOption1;
+    const { selectedOption2 } = this.state.selectedOption2;
     return (
       <div className="card-body">
         <h4 className="card-title">Brand</h4>
         <Select
+          value={selectedOption1}
           options={this.props.value1}
           onChange={this.handleChange}
-          value={selectedOption}
         />
         <h4 className="card-title">Series</h4>
         <Select options={this.props.value2} />
@@ -23,7 +57,11 @@ class CPU extends React.Component {
         <Select options={this.props.value3} />
         <br />
         <h4 className="card-title">CPU</h4>
-        <Select options={this.props.value4} />
+        <Select
+          value={selectedOption2}
+          options={this.filterData()}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
