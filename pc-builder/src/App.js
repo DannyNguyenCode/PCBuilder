777a2ac78e620842c41f7cda432f231b45807074
Gia Bao Nguyen <giabnguyen1@gmail.com>
2020-10-15 19:53:27 -0400

    Added layout for price rundown section

--- pc-builder/src/App.js
+++ pc-builder/src/App.js
@@ -7,6 +7,18 @@ import Card from './Card';
 import PriceRundown from './PriceRundown';
 
 class App extends React.Component {
+  state = {
+    cpuOption: 'Placeholder1',
+    moboOption: 'Placeholder2',
+    ramOption: 'Placeholder3',
+    caseOption: 'Placeholder4',
+    coolingOption: 'Placeholder5',
+    gpuOption: 'Placeholder6',
+    hddOption: 'Placeholder7',
+    ssdOption: 'Placeholder8',
+    psuOption: 'Placeholder9',
+  };
+
   render() {
     let computerParts = [
       'Central Processing Unit',
@@ -19,12 +31,13 @@ class App extends React.Component {
       'Solid State Drive',
       'Power Supply Unit',
     ];
+
     return (
       <div className='App'>
         <h1 className='app-header'>PC Builder</h1>
         <div className='container-fluid'>
           <div className='row'>
-            <div className='col-md leftLayout'>
+            <div className='col-md'>
               <div className='row'>
                 {computerParts.map((components, index) => {
                   return (
@@ -43,6 +56,34 @@ class App extends React.Component {
               </div>
             </div>
             {/*End of layout column col-md-6 */}
+          </div>
+          <div className='row d-flex justify-content-center'>
+            <h1 className='pricerundownheader'>Price Rundown</h1>
+          </div>
+          <div className='row'>
+            {/*Price run down table */}
+            <div className='col-md'>
+              <table class='table table-sm table-striped table-hover table-bordered'>
+                <thead>
+                  <tr>
+                    <th scope='col'>Part</th>
+                    <th scope='col'>Name</th>
+                    <th scope='col'>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {computerParts.map((component, index) => {
+                    return (
+                      <tr key={index}>
+                        <th scope='row'>{component}</th>
+                        <td>{this.state.cpuOption}</td>
+                        <td>$74.99</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
