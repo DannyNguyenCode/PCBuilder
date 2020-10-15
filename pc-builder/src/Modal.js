be4e269944fc7ecc85512329bc00bb966d9d4de1
Gia Bao Nguyen <giabnguyen1@gmail.com>
2020-10-15 13:31:45 -0400

    finished layout of modals with placeholder data

--- pc-builder/src/Modal.js
+++ pc-builder/src/Modal.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-export const Modal = () => (
-  <div>
+export const Modal = ({ component = 'No component provided' }) => (
+  <div className='modal-wrapper d-flex justify-content-center'>
     <button
       type='button'
       className='btn btn-light btn-outline-primary'
       data-toggle='modal'
-      data-target='#exampleModalCenter'
+      data-target={'#' + component}
     >
       Advanced Information &gt;&gt;
     </button>
 
     <div
       className='modal fade'
-      id='exampleModalCenter'
+      id={component}
       tabIndex='-1'
       role='dialog'
       aria-labelledby='exampleModalCenterTitle'
@@ -23,7 +23,7 @@ export const Modal = () => (
         <div className='modal-content'>
           <div className='modal-header'>
             <h5 className='modal-title' id='exampleModalLongTitle'>
-              Modal title
+              Advanced Information for {component}
             </h5>
             <button
               type='button'
@@ -34,7 +34,76 @@ export const Modal = () => (
               <span aria-hidden='true'>&times;</span>
             </button>
           </div>
-          <div className='modal-body'>...</div>
+          <div className='modal-body'>
+            <div className='container-fluid'>
+              <div className='row'>
+                <div className='col-3' style={{ border: '1px solid black' }}>
+                  Series
+                </div>
+                <div
+                  className='col-9'
+                  style={{ border: '1px solid black', borderLeft: 'none' }}
+                >
+                  tortor
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-3' style={{ border: '1px solid black' }}>
+                  faucibus
+                </div>
+                <div
+                  className='col-9'
+                  style={{ border: '1px solid black', borderLeft: 'none' }}
+                >
+                  at
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-3' style={{ border: '1px solid black' }}>
+                  turpis
+                </div>
+                <div
+                  className='col-9'
+                  style={{ border: '1px solid black', borderLeft: 'none' }}
+                >
+                  viverra
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-3' style={{ border: '1px solid black' }}>
+                  in
+                </div>
+                <div
+                  className='col-9'
+                  style={{ border: '1px solid black', borderLeft: 'none' }}
+                >
+                  adipiscing
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-3' style={{ border: '1px solid black' }}>
+                  eu mi
+                </div>
+                <div
+                  className='col-9'
+                  style={{ border: '1px solid black', borderLeft: 'none' }}
+                >
+                  at
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col-3' style={{ border: '1px solid black' }}>
+                  bibendum
+                </div>
+                <div
+                  className='col-9'
+                  style={{ border: '1px solid black', borderLeft: 'none' }}
+                >
+                  lacus
+                </div>
+              </div>
+            </div>
+          </div>
           <div className='modal-footer'>
             <button
               type='button'
@@ -42,9 +111,6 @@ export const Modal = () => (
               data-dismiss='modal'
             >
               Close
-            </button>
-            <button type='button' className='btn btn-primary'>
-              Save changes
             </button>
           </div>
         </div>
