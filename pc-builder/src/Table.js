6b75d2a8314dcf80d401099cb880c1eb909dac32
Gia Bao Nguyen <giabnguyen1@gmail.com>
2020-12-09 22:20:50 -0500

    implmented event handling to get data from Form component and populate table with data selected from user.

--- pc-builder/src/Table.js
+++ pc-builder/src/Table.js
@@ -1,5 +1,10 @@
 import React from 'react';
-
+/* do media queries for max and min width of each cell td
+          style={{
+            wordWrap: 'break-word',
+            minWidth: 200 + 'px',
+            maxWidth: 200 + 'px',
+          }}*/
 export const Table = ({
   computerParts,
   cpuOption,
@@ -11,8 +16,17 @@ export const Table = ({
   hddOption,
   ssdOption,
   psuOption,
+  cpuPrice,
+  motherboardsPrice,
+  ramsPrice,
+  casesPrice,
+  coolingsPrice,
+  gpusPrice,
+  hddsPrice,
+  ssdsPrice,
+  psusPrice,
 }) => (
-  <table className='table table-sm table-striped table-hover table-bordered'>
+  <table className='table table-sm table-responsive-sm table-striped table-hover table-bordered'>
     <thead>
       <tr>
         <th scope='col'>Part</th>
@@ -24,47 +38,63 @@ export const Table = ({
       <tr>
         <th scope='row'>{computerParts[0]}</th>
         <td>{cpuOption}</td>
-        <td>$74.99</td>
+        <td>{cpuPrice}</td>
       </tr>
       <tr>
         <th scope='row'>{computerParts[1]}</th>
         <td>{moboOption}</td>
-        <td>$74.99</td>
+        <td>{motherboardsPrice}</td>
       </tr>
       <tr>
         <th scope='row'>{computerParts[2]}</th>
         <td>{ramOption}</td>
-        <td>$74.99</td>
+        <td>{ramsPrice}</td>
       </tr>
       <tr>
         <th scope='row'>{computerParts[3]}</th>
         <td>{caseOption}</td>
-        <td>$74.99</td>
+        <td>{casesPrice}</td>
       </tr>
       <tr>
         <th scope='row'>{computerParts[4]}</th>
         <td>{coolingOption}</td>
-        <td>$74.99</td>
+        <td>{coolingsPrice}</td>
       </tr>
       <tr>
         <th scope='row'>{computerParts[5]}</th>
         <td>{gpuOption}</td>
-        <td>$74.99</td>
+        <td>{gpusPrice}</td>
       </tr>
       <tr>
         <th scope='row'>{computerParts[6]}</th>
         <td>{hddOption}</td>
-        <td>$74.99</td>
+        <td>{hddsPrice}</td>
       </tr>
       <tr>
         <th scope='row'>{computerParts[7]}</th>
         <td>{ssdOption}</td>
-        <td>$74.99</td>
+        <td>{ssdsPrice}</td>
       </tr>
       <tr>
         <th scope='row'>{computerParts[8]}</th>
         <td>{psuOption}</td>
-        <td>$74.99</td>
+        <td>{psusPrice}</td>
+      </tr>
+      <tr>
+        <th scope='row'>Total</th>
+        <td>Total Price of All Components Are:</td>
+        <td>
+          $
+          {Number(cpuPrice.substring(1)) +
+            Number(motherboardsPrice.substring(1)) +
+            Number(ramsPrice.substring(1)) +
+            Number(casesPrice.substring(1)) +
+            Number(coolingsPrice.substring(1)) +
+            Number(gpusPrice.substring(1)) +
+            Number(hddsPrice.substring(1)) +
+            Number(ssdsPrice.substring(1)) +
+            Number(psusPrice.substring(1))}
+        </td>
       </tr>
     </tbody>
   </table>
