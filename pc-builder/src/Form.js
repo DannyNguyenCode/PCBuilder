6976f7d16812561c5a305d0b6925365514b8ef20
Gia Bao Nguyen <giabnguyen1@gmail.com>
2020-12-09 22:18:48 -0500

    used event handlers to send data to parent components to populate Table component and removed dummy data, replaced with data from api

--- pc-builder/src/Form.js
+++ pc-builder/src/Form.js
@@ -1,93 +1,64 @@
 import React from 'react';
-{
-  /* dummy data in place of fetching api data 
-  remember to pass fetched data from parent component to child as an array of objects
-  */
-}
-let optionsTest = [
-  { name: 'Asdsadas', price: '$100' },
-  { name: 'Sdfgfghghg', price: '$200' },
-  { name: 'Erereretr', price: '$300' },
-  { name: 'Cvcvbbvbvbb', price: '$400' },
-  { name: 'Grbtbtbtbtb', price: '$500' },
-];
+
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { option: '', price: '', search: null };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = { priceOption: '' };
+    this.onTrigger = this.onTrigger.bind(this);
   }
-
-  handleChange(e) {
-    let option = optionsTest.find(
-      ({ name }) => name.toLowerCase() === e.target.value
+  onTrigger = (event) => {
+    let option = this.props.data.find(
+      ({ name }) => name.toLowerCase() === event.target.value
     );
     if (option === undefined) {
-      console.log('User must have selected default value');
       this.setState({ price: 'Select Name for Price' });
     } else {
-      this.setState({ option: option.name, price: option.price });
+      this.setState({ price: option.price });
+      this.props.cardCallBack(event, option.price);
     }
-  }
-
-  searchSpace = (event) => {
-    let keyword = event.target.value;
-    this.setState({ search: keyword });
   };
-
   render() {
-    const options = optionsTest
-      .filter((data) => {
-        if (this.state.search == null) {
-          return data;
-        } else if (
-          data.name.toLowerCase().includes(this.state.search.toLowerCase())
-        ) {
-          return data;
-        }
-      })
-      .map((data) => {
-        return (
-          <option id={data.name.toLowerCase()} value={data.name.toLowerCase()}>
-            {data.name}
-          </option>
-        );
-      });
-
+    const { selectid, data } = this.props;
     return (
-      <form className='form-inline d-flex justify-content-center'>
-        <input
-          className='form-control mr-sm-2 text-center'
-          id='search'
-          type='search'
-          placeholder='Search to Filter'
-          aria-label='Search'
-          onChange={(e) => this.searchSpace(e)}
-        />
+      <form>
         <div className='form-group'>
-          <label htmlFor='name'>Name</label>
+          <label htmlFor={selectid + 'Name'}>Name</label>
           <select
             className='form-control'
-            id='name'
-            onChange={this.handleChange}
+            id={selectid + 'Name'}
+            onChange={this.onTrigger}
           >
             <option defaultValue>Please select option</option>
-            {options}
+            {data &&
+              data.map((data, index) => {
+                return (
+                  <option
+                    key={index}
+                    id={data.name.toLowerCase()}
+                    value={data.name.toLowerCase()}
+                  >
+                    {data.name}
+                  </option>
+                );
+              })}
           </select>
-
+        </div>
+        {/* form-group */}
+        <div className='form-group'>
           <label htmlFor='price'>Price</label>
           <input
             className='form-control'
             type='text'
             placeholder='Select Name for Price'
             id='price'
-            value={this.state.price}
+            value={this.state.price || ''}
+            style={{ textAlign: 'center' }}
             disabled
           />
         </div>
+        {/* form-group */}
       </form>
     );
   }
 }
-
 export default Form;
