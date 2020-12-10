b9c2a05f9297b75fc7ec1dc63fc492e2872b80d1
Gia Bao Nguyen <giabnguyen1@gmail.com>
2020-12-09 22:13:29 -0500

    Made layout responsive using bootstrap4, added handle events for components to pass data to child and retreive data from child components, and added a section for recommended videos

--- pc-builder/src/App.js
+++ pc-builder/src/App.js
@@ -1,29 +1,71 @@
 import React from 'react';
 import './App.css';
-import Form from './Form';
-import { Modal } from './Modal';
 import { Table } from './Table';
-
-import Card from './Card';
-import PriceRundown from './PriceRundown';
+import CPU from './CardComponents/CPU';
+import Motherboard from './CardComponents/Motherboard';
+import RAM from './CardComponents/RAM';
+import Case from './CardComponents/Case';
+import Cooling from './CardComponents/Cooling';
+import GPU from './CardComponents/GPU';
+import HDD from './CardComponents/HDD';
+import SSD from './CardComponents/SSD';
+import PSU from './CardComponents/PSU';
 
 class App extends React.Component {
-  state = {
-    cpuOption: 'Placeholder1',
-    moboOption: 'Placeholder2',
-    ramOption: 'Placeholder3',
-    caseOption: 'Placeholder4',
-    coolingOption: 'Placeholder5',
-    gpuOption: 'Placeholder6',
-    hddOption: 'Placeholder7',
-    ssdOption: 'Placeholder8',
-    psuOption: 'Placeholder9',
-  };
-  handleSelect = (option) => {
-    this.setState({ cpuOption: option });
+  constructor(props) {
+    super(props);
+    this.state = {
+      cpuOptions: 'Please Select Central Processing Unit to populate table',
+      motherboardsOptions: 'Please Select Motherboard to populate table',
+      ramsOptions: 'Please Select Random Access Memory to populate table',
+      casesOptions: 'Please Select Cases to populate table',
+      coolingsOptions: 'Please Select Cooling to populate table',
+      gpusOptions: 'Please Select Graphics Processing Unit to populate table',
+      hddsOptions: 'Please Select Hard Disk Drive to populate table',
+      ssdsOptions: 'Please Select Solid State Drive to populate table',
+      psusOptions: 'Please Select Power Supply Unit to populate table',
+      cpuPrice: '$00.00',
+      motherboardsPrice: '$00.00',
+      ramsPrice: '$00.00',
+      casesPrice: '$00.00',
+      coolingsPrice: '$00.00',
+      gpusPrice: '$00.00',
+      hddsPrice: '$00.00',
+      ssdsPrice: '$00.00',
+      psusPrice: '$00.00',
+    };
+  }
+  handleCallback = (name, component, price) => {
+    if (component === 'cpu') {
+      this.setState({ cpuOptions: name.target.value, cpuPrice: price });
+    } else if (component === 'mobo') {
+      this.setState({
+        motherboardsOptions: name.target.value,
+        motherboardsPrice: price,
+      });
+    } else if (component === 'ram') {
+      this.setState({ ramsOptions: name.target.value, ramsPrice: price });
+    } else if (component === 'case') {
+      this.setState({ casesOptions: name.target.value, casesPrice: price });
+    } else if (component === 'cooling') {
+      this.setState({
+        coolingsOptions: name.target.value,
+        coolingsPrice: price,
+      });
+    } else if (component === 'gpu') {
+      this.setState({ gpusOptions: name.target.value, gpusPrice: price });
+    } else if (component === 'hdd') {
+      this.setState({ hddsOptions: name.target.value, hddsPrice: price });
+    } else if (component === 'ssd') {
+      this.setState({ ssdsOptions: name.target.value, ssdsPrice: price });
+    } else if (component === 'psu') {
+      this.setState({ psusOptions: name.target.value, psusPrice: price });
+    }
+    name.persist();
+    name.preventDefault();
   };
   render() {
-    let computerParts = [
+    const computerParts = [
       'Central Processing Unit',
       'Motherboard',
       'Random Access Memory',
@@ -37,57 +79,124 @@ class App extends React.Component {
 
     return (
       <div className='App'>
-        <h1 className='app-header'>PC Builder</h1>
-        <div className='container-fluid'>
-          <div className='row'>
-            <div className='col-md'>
-              <div className='row' id='card-wrapper'>
-                {computerParts.map((components, index) => {
-                  return (
-                    <div className='col-md-4' key={index} id={index}>
-                      <div className='card bg-light mb-3'>
-                        <div className='card-header'>{components}</div>
-                        <div className='card-body'>
-                          <Form
-                            component={components}
-                            onSelect={this.handleSelect}
-                          />
-                          <br />
-                          <Modal component={components} />
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
+        <nav
+          className='navbar navbar-dark bg-dark navbar-expand-sm fixed-top'
+          id='navbar-site'
+        >
+          <div className='container'>
+            <a className='navbar-brand' href='#top'>
+              PC Builder
+            </a>
+            <button
+              className='navbar-toggler'
+              type='button'
+              data-toggle='collapse'
+              data-target='#toggleNav'
+              aria-controls='toggleNav'
+              aria-expanded='false'
+              aria-label='Toggle Navigation'
+            >
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse' id='toggleNav'>
+              <div className='navbar-nav'>
+                <a className='nav-item nav-link' href='#parts'>
+                  Parts Selection
+                </a>
+                <a className='nav-item nav-link' href='#pricerundown'>
+                  Price Rundown
+                </a>
+                <a className='nav-item nav-link' href='#recommendedvids'>
+                  Recommended Videos
+                </a>
               </div>
+              {/* navbar-nav */}
             </div>
-            {/*End of layout column col-md-6 */}
+            {/* collapse navbar-collapse */}
+            <span className='navbar-text d-none d-xl-inline-block'>
+              For anyone looking to build a Desktop Computer
+            </span>
           </div>
-          <div className='row d-flex justify-content-center'>
-            <h1 className='pricerundownheader'>Price Rundown</h1>
+          {/* container */}
+        </nav>
+
+        <div className='container'>
+          <div className='row mx-0 mb-2 mt-4 justify-content-center'>
+            <div className='h4' id='parts' style={{ marginTop: 50 + 'px' }}>
+              Parts Selection
+            </div>
+            {/* h4 */}
           </div>
-          <div className='row' id='table'>
-            {/*Price run down table */}
-            <div className='col-md'>
-              <Table
-                computerParts={computerParts}
-                cpuOption={this.state.cpuOption}
-                moboOption={this.state.moboOption}
-                ramOption={this.state.ramOption}
-                caseOption={this.state.caseOption}
-                coolingOption={this.state.coolingOption}
-                gpuOption={this.state.gpuOption}
-                hddOption={this.state.hddOption}
-                ssdOption={this.state.ssdOption}
-                psuOption={this.state.psuOption}
-              />
+          {/* row mx-0 mb-2 mt-4 justify-content-center */}
+          <div className='row row-cols-sm-1 row-cols-md-2 row-cols-lg-3'>
+            <CPU key={computerParts[0]} parentCallback={this.handleCallback} />
+            <Motherboard
+              key={computerParts[1]}
+              parentCallback={this.handleCallback}
+            />
+            <RAM key={computerParts[2]} parentCallback={this.handleCallback} />
+            <Case key={computerParts[3]} parentCallback={this.handleCallback} />
+            <Cooling
+              key={computerParts[4]}
+              parentCallback={this.handleCallback}
+            />
+            <GPU key={computerParts[5]} parentCallback={this.handleCallback} />
+            <HDD key={computerParts[6]} parentCallback={this.handleCallback} />
+            <SSD key={computerParts[7]} parentCallback={this.handleCallback} />
+            <PSU key={computerParts[8]} parentCallback={this.handleCallback} />
+          </div>
+          {/* row row-cols-sm-2 row-cols-md-3 */}
+          <div className='row mx-0 mb-2 justify-content-center'>
+            <div className='h4' id='pricerundown'>
+              Price Rundown
             </div>
           </div>
-          <div className='row' id='total'>
-            <div className='col-md'></div>
+          {/* row mx-0 mb-2 justify-content-center */}
+          <div className='row mx-0 text-center mb-4'>
+            <Table
+              computerParts={computerParts}
+              cpuOption={this.state.cpuOptions}
+              moboOption={this.state.motherboardsOptions}
+              ramOption={this.state.ramsOptions}
+              caseOption={this.state.casesOptions}
+              coolingOption={this.state.coolingsOptions}
+              gpuOption={this.state.gpusOptions}
+              hddOption={this.state.hddsOptions}
+              ssdOption={this.state.ssdsOptions}
+              psuOption={this.state.psusOptions}
+              cpuPrice={this.state.cpuPrice}
+              motherboardsPrice={this.state.motherboardsPrice}
+              ramsPrice={this.state.ramsPrice}
+              casesPrice={this.state.casesPrice}
+              coolingsPrice={this.state.coolingsPrice}
+              gpusPrice={this.state.gpusPrice}
+              hddsPrice={this.state.hddsPrice}
+              ssdsPrice={this.state.ssdsPrice}
+              psusPrice={this.state.psusPrice}
+            />
           </div>
+          {/* row mx-0 text-center */}
+          <div className='row mx-0 mb-2 justify-content-center'>
+            <div className='h4' id='recommendedvids'>
+              Recommended Videos
+            </div>
+          </div>
+          {/* row mx-0 mb-2 justify-content-center */}
+          <div className='row mx-0 text-center'>
+            <div className='embed-responsive embed-responsive-16by9'>
+              <iframe
+                title='Sample'
+                className='embed-responsive-item'
+                src='https://www.youtube.com/embed/jLVGL7aAYgY'
+                frameBorder='0'
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+          {/* row mx-0 text-center */}
         </div>
-      </div>
+        {/* container */}
+      </div> /* App */
     );
   }
 }
