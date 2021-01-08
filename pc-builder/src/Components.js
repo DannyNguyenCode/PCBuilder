d8e813eae27165c92542882229a63ceb4e2474ad
Gia Bao Nguyen <giabnguyen1@gmail.com>
2021-01-08 09:17:52 -0500

    added error handling to fetching data

--- pc-builder/src/Components.js
+++ pc-builder/src/Components.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import Card from './Card';
 
-function Components({ parentCallback, component, ur, header }) {
+function Components({ parentCallback, component, url, header }) {
   const [option, setOptions] = useState(null);
+  const [error, setError] = useState(null);
   const handleCallBack = useCallback(
     (data, name, price) => {
       parentCallback(data, component, name, price);
@@ -10,16 +11,32 @@ function Components({ parentCallback, component, ur, header }) {
     },
     [parentCallback, component]
   );
-
-  const url = ur;
   useEffect(() => {
     fetch(url)
-      .then((res) => res.json())
-      .then((data) => setOptions(data));
+      .then(async (res) => {
+        if (res.ok) {
+          setOptions(await res.json());
+          setError(null);
+        } else {
+          setError(await res.text());
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
   }, [url]);
-
   return (
-    <Card header={header} data={option} componentCallBack={handleCallBack} />
+    <React.Fragment>
+      {error !== null ? (
+        <p>There was an errror fetching data for {header}</p>
+      ) : (
+        <Card
+          header={header}
+          data={option}
+          componentCallBack={handleCallBack}
+        />
+      )}
+    </React.Fragment>
   );
 }
 
